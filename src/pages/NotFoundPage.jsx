6bba2e9dcac2b833c5890954908eca79a650cf60
TizianoMu <73585178+TizianoMu@users.noreply.github.com>
2025-09-29 11:46:00 +0200

    Update NotFoundPage.jsx

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 const NotFoundPage = () => {
   return (
-    <div className="central-box">
+    <div className="central-box" style={{textAlign:"center"}}>
       <h1 className="title">404 - Page Not Found</h1>
       <p className="message">Sorry, the page you are looking for does not exist.</p>
       <a href="/" className="button">Torna alla Home</a>
