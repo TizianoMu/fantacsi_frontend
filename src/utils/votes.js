5394e5c002f9a082be45aeec3ff494a3cb90d05a
Tiziano Murzio <tizzyduemila@gmail.com>
2025-10-01 17:49:38 +0200

    wip

--- src/utils/votes.js
+++ src/utils/votes.js
@@ -1,4 +1,4 @@
-const getVoteClass = (vote) => {
+export const getVoteClass = (vote) => {
     if (vote === null) return 'vote-sv'; // Senza Voto
     if (vote >= 6.5) return 'vote-good';
     if (vote >= 6.0) return 'vote-sufficient';
